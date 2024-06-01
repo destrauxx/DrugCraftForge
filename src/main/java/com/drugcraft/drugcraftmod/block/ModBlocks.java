d66a19cc1ef33ace25bfdbbd664dc75998dcd351
destrauxx <gleroapachii@gmail.com>
2024-06-01 21:25:18 +0300

    added wild marijuana

--- src/main/java/com/drugcraft/drugcraftmod/block/ModBlocks.java
+++ src/main/java/com/drugcraft/drugcraftmod/block/ModBlocks.java
@@ -2,11 +2,13 @@ package com.drugcraft.drugcraftmod.block;
 
 import com.drugcraft.drugcraftmod.DrugCraftMod;
 import com.drugcraft.drugcraftmod.block.custom.MarijuanaCrop;
+import com.drugcraft.drugcraftmod.item.ModCreativeModeTab;
 import com.drugcraft.drugcraftmod.item.ModItems;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.TallFlowerBlock;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -22,6 +24,8 @@ public class ModBlocks {
 
     public static final RegistryObject<Block> MARIJUANA_CROP = BLOCKS.register("marijuana_crop",
             () -> new MarijuanaCrop(BlockBehaviour.Properties.copy(Blocks.WHEAT).noCollission()));
+    public static final RegistryObject<Block> WILD_MARIJUANA_CROP = registerBlock("wild_marijuana_crop",
+            () -> new TallFlowerBlock(BlockBehaviour.Properties.copy(Blocks.ROSE_BUSH).noCollission()));
 
     private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
         RegistryObject<T> toReturn = BLOCKS.register(name, block);
@@ -31,7 +35,7 @@ public class ModBlocks {
 
     private static <T extends Block> RegistryObject<Item> registerBlockItem(String name, RegistryObject<T> block) {
         return ModItems.ITEMS.register(name, () -> new BlockItem(block.get(),
-                new Item.Properties()));
+                new Item.Properties().tab(ModCreativeModeTab.DRUGCRAFT_TAB)));
     }
 
     public static void register(IEventBus eventBus) {
